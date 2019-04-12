a0238af9a2930285e33378c6df4695fd25e1e81e
laimison <laimison1@gmail.com>
2019-04-12 18:29:07 +0100

    initial nginx reverse proxy container started successfully

--- nginx-reverse/run.sh
+++ nginx-reverse/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-docker stop my-reverse
-docker rm my-reverse
+docker image pull nginx:1.15.11
+docker stop my-reverse 2>/dev/null
+docker rm my-reverse 2>/dev/null
 docker build -f Dockerfile -t my-reverse-image .
-# docker run -itd -p 80:80 -p 443:443 --name my-reverse -v "$PWD/html":/var/www/html my-reverse-image
+docker run -itd -p 80:80 -p 443:443 --name my-reverse -v "$PWD/html":/usr/share/nginx/html my-reverse-image
 docker ps
 # curl http://localhost
 # curl -k https://localhost
